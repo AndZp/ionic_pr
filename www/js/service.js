80234fcb4f5144b1d70240aee46e973de29f6bfe
AndZp <andrey.oleynik.zp@gmail.com>
2015-02-18 12:34:01 +0200

    + отправка сообщений на парс.  (поля From и To связаны с табл User)

--- www/js/service.js
+++ www/js/service.js
@@ -2,13 +2,39 @@
  * Created by User on 16.02.2015.
  */
 angular.module('ionicParseApp.service', [])
-.service ('geoPoints', function Geopoints() {
+    .service('geoPoints', function Geopoints() {
     var geopoints = this;
     geopoints.arrUsers = [];
 
 })
-.service ('chatService', function chatService() {
+    .service('chatService', function chatService() {
     var chatService = this;
     chatService.chatTo = "";
 
-});
+    })
+
+    .service('parseService', function parseService() {
+        var parseService = this;
+        parseService.loginUser;
+
+        ChatTab = Parse.Object.extend("chat");
+        parseService.sendToParse = function (messageChatParse) {
+
+            messageChatParse.save(null, {
+                success: function (messageChatParse) {
+                    // Execute any logic that should take place after the object is saved.
+                    console.log('New object created with objectId: ' + messageChatParse.id);
+                    //$scope.id =  messageChatParse.id;
+
+                },
+                error: function (messageChatParse, error) {
+                    // Execute any logic that should take place if the save fails.
+                    // error is a Parse.Error with an error code and message.
+                    console.log('Failed to create new object, with error code: ' + error.message);
+                }
+            });
+
+        };
+
+    })
+;
